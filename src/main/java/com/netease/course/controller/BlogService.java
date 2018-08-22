bcbf1345cff8d4f97c0350ef2ba11858dcb0de8d
XIAOFANG <XIAOFANG@LAPTOP-3L7PPJ0R>
2018-08-22 23:27:14 +0800

    test

--- src/main/java/com/netease/course/controller/BlogService.java
+++ src/main/java/com/netease/course/controller/BlogService.java
@@ -1,8 +1,6 @@
 package com.netease.course.controller;
 
-/**
- * Created by gigabyte on 2016-06-21.
- */
+
 public interface BlogService {
     public void pressblog(String title,String content);
 }
