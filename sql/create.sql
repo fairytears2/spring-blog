88bb5e4289ac37d6a80e0f14e802cd12b30d8b83
XIAOFANG <XIAOFANG@LAPTOP-3L7PPJ0R>
2018-08-22 23:36:39 +0800

    text

---
+++ sql/create.sql
@@ -0,0 +1,14 @@
+########################
+# Drop all table
+########################
+
+
+
+CREATE TABLE blog
+(
+  blogTitle       VARCHAR(10),
+  blogContent     VARCHAR(100),
+  nowDate         VARCHAR(255),
+)
+
+
